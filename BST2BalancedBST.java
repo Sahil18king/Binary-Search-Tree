139b5125ee045e2fb58dca366b0d908dbdc61e38
SAHIL SONI <133500990+Sahil18king@users.noreply.github.com>
2023-11-07 22:11:46 +0530

    Create BST2BalancedBST.java

---
+++ BST2BalancedBST.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class BST2 {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data){
+            this.data = data;
+            this.right = null;
+            this.left = null;
+        }
+    }
+  //preorder
+    public static void Preorder(Node root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        Preorder(root.left);
+        Preorder(root.right);
+    }
+  //inorder , stored in ArrayList
+
+    public static void getInorder(Node root,ArrayList<Integer> inorder ){
+        if(root == null){
+            return;
+        }
+        getInorder(root.left, inorder);
+        inorder.add(root.data);
+        getInorder(root.right, inorder);
+    }
+    public static Node createBST (ArrayList<Integer> inorder, int st, int end){
+        if(st > end){
+            return null ;
+        }
+
+        int mid = (st+end)/2;
+        Node root = new Node (inorder.get(mid));
+        root.left = createBST(inorder, st, mid-1);
+        root.right = createBST(inorder, mid+1, end);
+        return root;
+
+    }
+    //Bst to balanced bst
+    public static Node balancedBST(Node root){
+        //inorder seq
+        ArrayList<Integer> inorder = new ArrayList<>();
+        getInorder(root, inorder);
+
+        //sorted inorder -> balanced BST
+        root = createBST(inorder,0 , inorder.size()-1);
+        return root;
+    }
+    public static void main(String args[]){
+        Node root =new Node(8);
+        root.left = new Node(6);
+        root.left.left= new Node(5);
+        root.left.left.left = new Node(3);
+
+        root.right = new Node(10);
+        root.right.right = new Node(11);
+        root.right.right.right = new Node(12);
+       root = balancedBST(root);
+       Preorder(root);
+    }
+}
